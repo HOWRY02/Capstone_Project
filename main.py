f61db1055aa57e57928fa04dcb02e89bed7a0762
Phuc <huynhvinhphuc101@gmail.com>
2024-06-05 09:16:45 +0700

    update main

--- main.py
+++ main.py
@@ -190,4 +190,4 @@ async def insert_data(data: JSONData):
 # Run the application (optional, for development purposes)
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
