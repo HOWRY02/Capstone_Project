792976bb1e0b83c2c77c75b2d49e2254ce9fae40
Phuc <huynhvinhphuc101@gmail.com>
2024-06-17 20:12:11 +0700

    optimize extraction info js

--- src/WEB/resources/script/extract_info/main.js
+++ src/WEB/resources/script/extract_info/main.js
@@ -44,38 +44,29 @@ tempImg.onload = function () {
 };
 tempImg.src = imgSrc;
 
-boxes = boxesTemp.map(data => ({
-    startX: data.box[2],
-    startY: data.box[3],
-    width: data.box[0] - data.box[2],
-    height: data.box[1] - data.box[3],
-    text: data.text || '', // Ensure text property exists or set it to an empty string
-    class: data.class,
-    answer_text: data.answer_text.map(answer_data => ({
+boxes = boxesTemp.map(data => {
+    let loc;
+    
+    loc = [{
+        startX: data.box[2],
+        startY: data.box[3],
+        width: data.box[0] - data.box[2],
+        height: data.box[1] - data.box[3],
+        text: data.text || '', // Ensure text property exists or set it to an empty string
+        class: data.class
+    }]
+
+    loc = loc.concat(data.answer_text.map(answer_data => ({
         startX: answer_data.box[2],
         startY: answer_data.box[3],
         width: answer_data.box[0] - answer_data.box[2],
         height: answer_data.box[1] - answer_data.box[3],
         text: answer_data.text || '',
         class: answer_data.class
-    }))
-}));
+    })))
 
-// for (const dataTemp of boxesTemp) {
-//     // console.log(dataTemp)
-//     answer_boxes = answer_boxes.concat(dataTemp.answer_text.map(data => ({
-//         startX: data.box[2],
-//         startY: data.box[3],
-//         width: data.box[0] - data.box[2],
-//         height: data.box[1] - data.box[3],
-//         text: data.text || '', // Ensure text property exists or set it to an empty string
-//         class: data.class,
-//         // ocr_text: data.ocr_text
-//     })));
-// }
-// boxes = boxes.concat(answer_boxes)
-console.log(boxes)
-// console.log(answer_boxes)
+    return loc;
+});
 
 // Adding an event listener to 'imageInput' element when a file is selected
 imageInput.addEventListener('change', function (event) {
@@ -133,27 +124,29 @@ imageCanvas.addEventListener('mousedown', (e) => {
         // If a resize action is detected
         isDragging = false;
         isResizing = true;
-        resizingBox = boxes[boxIndex]; // Getting the box to be resized
+        resizingBox = boxes[boxIndex[0]][boxIndex[1]]; // Getting the box to be resized
         selectedBox = resizingBox; // Selecting the box when clicking on the resize handle
     } else if (type === 'drag') {
         // If a drag action is detected
         isDragging = true;
-        selectedBox = boxes[boxIndex]; // Selecting the box to be dragged
+        isResizing = false;
+        selectedBox = boxes[boxIndex[0]][boxIndex[1]]; // Selecting the box to be dragged
     } else {
         // If neither resize nor drag action is detected
-        if (selectedBox !== null && selectedBox === boxes[boxIndex]) {
+        if (selectedBox === boxes[boxIndex[0]]) {
             // If the clicked box is already selected, deselect it
             selectedBox = null;
         } else {
             // If a new box creation action is detected
-            isDragging = true;
+            isResizing = true;
             // Creating a new box at the clicked position with initial dimensions
             const box = {
                 startX: Math.round(mouseX), startY: Math.round(mouseY),
                 width: 0, height: 0, class: currentMode
             };
-            boxes.push(box); // Adding the new box to the boxes array
-            selectedBox = box; // Setting the newly created box as selected
+            boxes.push([box]); // Adding the new box to the boxes array
+            resizingBox = box;
+            selectedBox = resizingBox; // Setting the newly created box as selected
         }
     }
     draw();
@@ -168,24 +161,20 @@ imageCanvas.addEventListener('mousemove', (e) => {
     mouseX = e.clientX - rect.left;
     mouseY = e.clientY - rect.top;
 
-    // Checking if dragging action is ongoing
-    if (isDragging) {
-        // Handling box movement or resizing based on the action type
-        if (selectedBox !== null && !isResizing) {
-            // If dragging a box (not resizing)
-            const dx = mouseX - selectedBox.startX;
-            const dy = mouseY - selectedBox.startY;
-
+    // Handling box movement or resizing based on the action type
+    if (selectedBox !== null) {
+        if (isDragging && selectedBox !== null) {
             // Updating box position and dimensions based on mouse movement
-            selectedBox.startX = Math.round(mouseX);
-            selectedBox.startY = Math.round(mouseY);
-            selectedBox.width -= Math.round(dx);
-            selectedBox.height -= Math.round(dy);
+            selectedBox.startX = Math.round(mouseX - selectedBox.width);
+            selectedBox.startY = Math.round(mouseY - selectedBox.height);
         } else if (isResizing && resizingBox !== null) {
-            // If resizing a box
-            // Calculating and updating the resized box dimensions based on mouse movement
-            resizingBox.width = Math.round(Math.max(0, mouseX - resizingBox.startX));
-            resizingBox.height = Math.round(Math.max(0, mouseY - resizingBox.startY));
+            const dx = mouseX - resizingBox.startX;
+            const dy = mouseY - resizingBox.startY;
+            // Updating box position and dimensions based on mouse movement
+            resizingBox.startX = Math.round(mouseX);
+            resizingBox.startY = Math.round(mouseY);
+            resizingBox.width -= Math.round(dx);
+            resizingBox.height -= Math.round(dy);
         }
         draw();
     }
@@ -236,15 +225,21 @@ document.addEventListener('keydown', (e) => {
     if (e.key === 'Delete') {
         // Check if a box is currently selected
         if (selectedBox !== null) {
-            // Find the index of the selected box in the boxes array
-            const index = boxes.indexOf(selectedBox);
-            // If the selected box is found in the array
-            if (index !== -1) {
-                // Remove the selected box from the boxes array
-                boxes.splice(index, 1);
-                selectedBox = null; // Clear the selectedBox reference
-                draw();
+            for (let i = 0; i < boxes.length; i++) {
+                // Find the index of the selected box in the boxes array
+                const index = boxes[i].indexOf(selectedBox);
+                // If the selected box is found in the array
+                if (index !== -1) {
+                    // Remove the selected box from the boxes array
+                    boxes[i].splice(index, 1);
+                    selectedBox = null; // Clear the selectedBox reference
+                }
+
+                if (boxes[i].length === 0) {
+                    boxes.splice(i, 1);
+                }
             }
+            draw();
         }
     }
 });
