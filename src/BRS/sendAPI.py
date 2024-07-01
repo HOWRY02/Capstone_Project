29e6fc5eeeff26758675deeb90b1c46152c6f0f7
Phuc <huynhvinhphuc101@gmail.com>
2024-07-01 14:54:51 +0700

    update path runScanner.sh

--- src/BRS/sendAPI.py
+++ src/BRS/sendAPI.py
@@ -3,22 +3,6 @@ import subprocess
 
 # Execute the command with error handling and output redirection
 try:
-    result = subprocess.run(["sh", "./runScanner.sh"], stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL )
+    result = subprocess.run(["sh", "./src/BRS/runScanner.sh"], stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL )
 except subprocess.CalledProcessError as error:
     print(f"Error running command: {error}")
-
-
-'''with open("images/test.png", 'rb') as f:
-    img_data = f.read
-    
-url = ""
-headers = {"": ""} 
-
-r = requests.post(url, headers=headers)
-
-files = {"image": ("test.png", img_data)}
-
-r = requests.post(url, headers=headers, files=files)
-
-if r.status_code == 201:
-    print("Image uploaded successfully!")'''
