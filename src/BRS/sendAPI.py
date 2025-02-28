6ccddd62ec47d1b07acda074ebf5c0029b8df7d8
Phuc <huynhvinhphuc101@gmail.com>
2025-02-28 22:41:23 +0700

    update Scanner

--- src/BRS/sendAPI.py
+++ src/BRS/sendAPI.py
@@ -3,6 +3,6 @@ import subprocess
 
 # Execute the command with error handling and output redirection
 try:
-    result = subprocess.run(["sh", "./src/BRS/runScanner.sh"], stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL )
+    result = subprocess.run(["sh", "./src/BRS/runScanner.sh"], stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
 except subprocess.CalledProcessError as error:
     print(f"Error running command: {error}")
